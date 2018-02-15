ea514818bca8c58d4a7adda6d21ca11fa920e401
cotestatnt <cotestatnt@yahoo.com>
2018-02-15 10:25:34 +0100

    Add files via upload

--- data/www/auth/tools.js
+++ data/www/auth/tools.js
@@ -47,21 +47,23 @@ function listCONF(obj) {
   document.getElementById('nodeNumber').value = obj.nodeNumber;
   document.getElementById('zone1').value = obj.zone1;
   document.getElementById('zone2').value = obj.zone2;
-  document.getElementById('startTimeZ1').value = obj.startTimeZ1;
-  document.getElementById('stopTimeZ1').value = obj.stopTimeZ1;
-  document.getElementById('startTimeZ2').value = obj.startTimeZ2;
-  document.getElementById('stopTimeZ2').value = obj.stopTimeZ2;
+  document.getElementById('startZ1').value = obj.startZ1;
+  document.getElementById('stopZ1').value = obj.stopZ1;
+  document.getElementById('startZ2').value = obj.startZ2;
+  document.getElementById('stopZ2').value = obj.stopZ2;
   
-  document.getElementById('warning1').checked = obj.warning1;
-  document.getElementById('alarm1').checked = obj.alarm1;
-  document.getElementById('warning2').checked = obj.warning2;
-  document.getElementById('alarm2').checked = obj.alarm2;
-  
+  document.getElementById('n1').checked = obj.n1;
+  document.getElementById('p1').checked = obj.p1;
+  document.getElementById('a1').checked = obj.a1;
+  document.getElementById('n2').checked = obj.n2;
+  document.getElementById('p2').checked = obj.p2;
+  document.getElementById('a2').checked = obj.a2;
+
   document.getElementById('pauseTime').value = obj.pauseTime;
   document.getElementById('hornTime').value = obj.hornTime;
   
-  document.getElementById('phoneNumber1').value = obj.phoneNumber1;
-  document.getElementById('phoneNumber2').value = obj.phoneNumber2;
+  document.getElementById('phone1').value = obj.phone1;
+  document.getElementById('phone2').value = obj.phone2;
   document.getElementById('pushDevId').value = obj.pushDevId;
   
   var wifiSetup = !obj.dhcp;
@@ -100,21 +102,23 @@ function saveConf() {
   datatosend.nodeNumber = document.getElementById('nodeNumber').value;
   datatosend.zone1 = document.getElementById('zone1').value;  
   datatosend.zone2 = document.getElementById('zone2').value;  
-  datatosend.stopTimeZ1 = document.getElementById('stopTimeZ1').value;
-  datatosend.startTimeZ1 = document.getElementById('startTimeZ1').value;
-  datatosend.stopTimeZ2 = document.getElementById('stopTimeZ2').value;
-  datatosend.startTimeZ2 = document.getElementById('startTimeZ2').value;
+  datatosend.stopZ1 = document.getElementById('stopZ1').value;
+  datatosend.startZ1 = document.getElementById('startZ1').value;
+  datatosend.stopZ2 = document.getElementById('stopZ2').value;
+  datatosend.startZ2 = document.getElementById('startZ2').value;
   
-  datatosend.warning1 = document.getElementById('warning1').checked;
-  datatosend.alarm1 = document.getElementById('alarm1').checked;
-  datatosend.warning2 = document.getElementById('warning2').checked;
-  datatosend.alarm2 = document.getElementById('alarm2').checked;
+  datatosend.n1 = document.getElementById('n1').checked;
+  datatosend.p1 = document.getElementById('p1').checked;
+  datatosend.a1 = document.getElementById('a1').checked;
+  datatosend.n2 = document.getElementById('n2').checked;
+  datatosend.p2 = document.getElementById('p2').checked;
+  datatosend.a2 = document.getElementById('a2').checked;
    
   datatosend.pauseTime = document.getElementById('pauseTime').value;
   datatosend.hornTime = document.getElementById('hornTime').value;
   
-  datatosend.phoneNumber1 = document.getElementById('phoneNumber1').value;
-  datatosend.phoneNumber2 = document.getElementById('phoneNumber2').value;
+  datatosend.phone1 = document.getElementById('phone1').value;
+  datatosend.phone2 = document.getElementById('phone2').value;
   datatosend.pushDevId = document.getElementById('pushDevId').value;
   
   console.log(JSON.stringify(datatosend));
